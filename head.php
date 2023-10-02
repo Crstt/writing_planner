6dee8be2d80894944de3a3e313e9b3f84a774ef7
Crstt <matteo598.catalano@gmail.com>
2023-10-02 10:22:21 -0400

    Adds version

--- head.php
+++ head.php
@@ -6,12 +6,12 @@
 		<?php echo $title; ?>
 	</title>
 	<meta charset="utf-8" />
+	<meta name="version" content="0.2">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="cae.css" />
 	<script type="text/javascript" src="./js/Modernizr-2.5.3.forms.js"></script>
-	<script type="text/javascript" src="./js/html5Forms.js" data-webforms2-support="date"
-		data-webforms2-force-js-validation="true" data-lang="qq"></script>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script type="text/javascript" src="./js/html5Forms.js" data-webforms2-support="date" data-webforms2-force-js-validation="true" data-lang="qq"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<!-- Bootstrap CSS & JS -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
